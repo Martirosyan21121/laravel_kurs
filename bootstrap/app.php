61629e68aff94ccb49c886bcfe21f3f31ac12e81
Narek Martirosyan <narekmartirosyanbm21@gmail.com>
2024-06-03 13:00:16 +0400

    middleware (admin, user,auth)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -23,5 +23,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-
+        $exceptions->render(function (){
+            // for 404, 500, and for all
+            return redirect()->route('error')->with('error', 'You need to be logged in as a user to access this page.');
+        });
     })->create();
