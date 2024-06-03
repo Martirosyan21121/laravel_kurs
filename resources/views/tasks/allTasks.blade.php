98574cf7cee62eff40f1974277a619917616aaea
Narek Martirosyan <narekmartirosyanbm21@gmail.com>
2024-06-03 17:55:40 +0400

    add Tasks

--- resources/views/tasks/allTasks.blade.php
+++ resources/views/tasks/allTasks.blade.php
@@ -9,9 +9,52 @@
 <body>
 <div class="container">
     <h2  style="margin-top: 10px; text-align: center">All Tasks</h2>
-   <table class="">
-
-   </table>
+    <table class="table table-striped table-dark">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Status</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Updated at</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
 </div>
 </body>
 </html>
